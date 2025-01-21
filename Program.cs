94b2bef3b86bf00793e91cbc69eb0600a293b949
PauloMatiasxvs <155742553+PauloMatiasxvs@users.noreply.github.com>
2025-01-21 12:01:55 -0300

    Contoso Petting Zoo

--- Program.cs
+++ Program.cs
@@ -1,17 +1,43 @@
-﻿double usd = 23.73;
-int vnd = UsdToVnd(usd);
+﻿using System;
 
-Console.WriteLine($"${usd} USD = ${vnd} VND");
-Console.WriteLine($"${vnd} VND = ${VndToUsd(vnd)} USD");
-
-int UsdToVnd(double usd) 
+string[] pettingZoo = 
 {
-    int rate = 23500;
-    return (int) (rate * usd);
+    "alpacas", "capybaras", "chickens", "ducks", "emus", "geese", 
+    "goats", "iguanas", "kangaroos", "lemurs", "llamas", "macaws", 
+    "ostriches", "pigs", "ponies", "rabbits", "sheep", "tortoises",
+};
+
+RandomizeAnimals();
+string[,] group = AssignGroup();
+Console.WriteLine("School A");
+// PrintGroup(group);
+
+void RandomizeAnimals() 
+{
+    Random random = new Random();
+
+    for (int i = 0; i < pettingZoo.Length; i++) 
+    {
+        int r = random.Next(i, pettingZoo.Length);
+
+        string temp = pettingZoo[r];
+        pettingZoo[r] = pettingZoo[i];
+        pettingZoo[i] = temp;
+    }
 }
 
-double VndToUsd(int vnd) 
+string[,] AssignGroup(int groups = 6) 
 {
-    double rate = 23500;
-    return vnd / rate;
+    string[,] result = new string[groups, pettingZoo.Length/groups];
+    int start = 0;
+
+    for (int i = 0; i < groups; i++) 
+    {
+        for (int j = 0; j < result.GetLength(1); j++) 
+        {
+            result[i,j] = pettingZoo[start++];
+        }
+    }
+
+    return result;
 }
